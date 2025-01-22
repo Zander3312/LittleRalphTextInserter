5ed070ed2adf25b086382fe98d35fba8566823b6
Zander <zanderbhesch@gmail.com>
2025-01-22 11:00:24 -0500

    added support for all text

--- LittleRalphTextInserter.cpp
+++ LittleRalphTextInserter.cpp
@@ -7,25 +7,13 @@
 #include "LittleRalphTextInserter.h"
 using namespace std;
 
-string hexToAddress(string hex) { //Converts a hex value to an address that can be inserted into the game, which involves changing to little endian and adding 0x80000000. Currently unused.
-    string newHex{ "80" };
-    while (hex.length() < 6) {
-        hex.insert(0, "0");
-    }
-    newHex.insert(0, hex.substr(0, 2));
-    newHex.insert(0, hex.substr(2, 2));
-    return newHex.insert(0, hex.substr(4, 2));
-}
-
 string intToAddress(int num) { //Converts an int to an address that can be inserted into the game, which involves changing to hex in little endian and adding 0x80000000.
     string newHex{ "80" };
     stringstream stream{};
     stream << hex << num;
     string hexStr{stream.str()};
-    while (hexStr.length() < 6) {
+    while (hexStr.length() < 6)
         hexStr.insert(0, "0");
-    }
-
     newHex.insert(0, hexStr.substr(0, 2));
     newHex.insert(0, hexStr.substr(2, 2));
     return newHex.insert(0, hexStr.substr(4, 2));
@@ -107,10 +95,9 @@ int main(int argc, char* argv[])
 
                     if (boxText.length() > charSpace) { //figure out where there is space for the text and write characters there
                         if (boxText.length() > extraSpace) { //completely out of space, could potentially add more extra space locations but I don't anticipate needing them
-                            cerr << "Error: Out of space. Consider rewriting text to be shorter." << endl;
+                            cerr << "Error: Out of space. Consider rewriting text to be shorter.\nWrote successfully to first " << numBoxes << " text boxes." << endl;
                             return 1;
                         }
-
                         writeFile.seekp(extraLocation, ios::beg); //not enough space in the main character area, so go to where there's free space
                         writeFile.write(boxText.c_str(), boxText.length()); //write characters
                         address = intToAddress(extraLocation + 845120); //get address, add 845120 (0xCE540) to account for position in RAM
@@ -124,7 +111,6 @@ int main(int argc, char* argv[])
                         charLocation = charLocation + boxText.length(); //move where cursor will start later
                         charSpace = charSpace - boxText.length(); //update how much character space we have left
                     }
-
                     //write the properties of the text box to the correct location; horizontal positioning is fucky, should fix that later
                     writeFile.seekp(propLocation, ios::beg); //move cursor to the text box properties
                     writeFile.write(hexToInsertable(address).c_str(), 4); //write the address of the characters
@@ -173,14 +159,12 @@ int main(int argc, char* argv[])
                         }
                         prevDots = 0;
                     }
-
                     charsOnLine++;
 
                     if (currentChar == '/') { //clear text box character, clears characters in box but doesn't start a new box
                         lines = 1;
                         charsOnLine = 0;
                     }
-
                     maxCharsOnLine = max(charsOnLine, maxCharsOnLine);
                     boxText.push_back(static_cast<unsigned char>(charMap[currentChar])); //add character to the string, will add all in the text box at once after we verify there's space for it
                 }
@@ -200,7 +184,7 @@ int main(int argc, char* argv[])
                 charsOnLine = 0;
             }
         } //while end, should only reach this if there are less text boxes given than there are in the stage
-        cout << "Wrote to some text boxes, but not all. If this was not intended, you probably missed a box somewhere." << endl;
+        cout << "Wrote to " << numBoxes <<  " text boxes, but not all. If this was not intended, you probably missed a box somewhere." << endl;
         textFile.close();
         writeFile.close();
         return 0;
